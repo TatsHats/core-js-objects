9d21afe35358adedbde7609c82d2d7e6e996ba87
TatsHats <6642017@gmail.com>
2025-02-27 19:44:55 +0200

    feat: 12 passing (Чт, фев 27, 2025 07:44:55 )

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -32,8 +32,16 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const res = {};
+
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      res[key] = (res[key] || 0) + value;
+    });
+  });
+
+  return res;
 }
 
 /**
@@ -49,8 +57,14 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const res = { ...obj };
+
+  keys.forEach((key) => {
+    delete res[key];
+  });
+
+  return res;
 }
 
 /**
@@ -65,8 +79,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return Object.entries(obj1).every(([key, value]) => obj2[key] === value);
 }
 
 /**
@@ -80,8 +94,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -100,8 +114,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -114,8 +128,21 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  let len = 0;
+
+  const allPos = Object.values(lettersObject).flat();
+  len = allPos.length > 0 ? Math.max(...allPos) : 0;
+
+  const arr = new Array(len + 1).fill('');
+
+  Object.entries(lettersObject).forEach(([letter, positions]) => {
+    positions.forEach((pos) => {
+      arr[pos] = letter;
+    });
+  });
+
+  return arr.join('');
 }
 
 /**
@@ -132,8 +159,33 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let money25 = 0;
+  let money50 = 0;
+  let getSell = true;
+
+  queue.forEach((money) => {
+    if (money === 25) {
+      money25 += 1;
+    } else if (money === 50) {
+      if (!money25) {
+        getSell = false;
+      }
+      money25 -= 1;
+      money50 += 1;
+    } else if (money === 100) {
+      if (money50 && money25) {
+        money25 -= 1;
+        money50 -= 1;
+      } else if (money25 >= 3) {
+        money25 -= 3;
+      } else {
+        getSell = false;
+      }
+    }
+  });
+
+  return getSell;
 }
 
 /**
@@ -149,8 +201,13 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+
+  this.getArea = function getArea() {
+    return this.width * this.height;
+  };
 }
 
 /**
